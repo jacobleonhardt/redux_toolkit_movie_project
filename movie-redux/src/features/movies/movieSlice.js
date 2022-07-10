95f1ba0e371781c1455a96afe61850f01067886c
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-07-09 20:46:07 -0500

    make async thunk for fetching movie

---
+++ movie-redux/src/features/movies/movieSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const key = apiInfo.SECRET_KEY
+const initialState = []
+
+export const getMovie = createAsyncThunk(
+  'movies/getMovie',
+  async (title) => {
+    try {
+      const searchableTitle = title.join('-').toLowerCase()
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': key,
+          'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
+        }
+      }
+
+      const res = await fetch(
+        `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${searchableTitle}&country=us`,
+        options
+      )
+      const data = await res.json()
+      return data
+    } catch (err) {
+      return err.message
+    }
+  }
+)
+
+export const moviesSlice = createSlice({
+  name: 'movies',
+  initialState,
+  reducers: {},
+})
+
+export default moviesSlice.reducer
