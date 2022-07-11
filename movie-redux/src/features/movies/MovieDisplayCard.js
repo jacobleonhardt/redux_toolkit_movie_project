d9913ae80281231ecec11109094129a1e66a6ecf
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-07-11 17:08:04 -0500

    add styles to image and streaming providers

--- movie-redux/src/features/movies/MovieDisplayCard.js
+++ movie-redux/src/features/movies/MovieDisplayCard.js
@@ -4,8 +4,10 @@ export const MovieDisplayCard = (item) => {
   return (
     <div className='list-item-card'>
         <img src={item.item.picture} alt='movie or tv show stillframe' />
-        <span>{item.item.name}</span>
-        {item.item.locations.map( provider => <img key={provider.id} src={provider.icon} alt='streaming provider icon' />)}
+        <h3>{item.item.name}</h3>
+        <div className='provider-icons'>
+          {item.item.locations.map( provider => <img key={provider.id} src={provider.icon} alt='streaming provider icon' />)}
+        </div>
     </div>
   )
 }
